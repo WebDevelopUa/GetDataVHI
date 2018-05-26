38561765944dc92b0bd596f78005e1f7f4323339
WebDev <js.develop.com@gmail.com>
2018-05-26 16:03:36 +0300

    Add info

--- get_url_data_kiev.py
+++ get_url_data_kiev.py
@@ -10,17 +10,17 @@ import datetime
 # URL-адрес Киева (ID=11)
 url = "https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_provinceData.php?country=UKR&provinceID=11&year1=1981&year2=2018&type=Mean"
 
+print "Данные загружаются с сайта ... Примерное время ожидания - до 5 минут"
+
 # функция urlopen() способна извлечь URL-адрес с помощью различных протоколов (HTTP, FTP)
 vhi_url = urllib2.urlopen(url)
 
-print "Данные загружаются с сайта ... Примерное время ожидания 1 минута"
-
 # откроем файл на запись (со скачиванием бинарного файла 'b') для Киева (ID=11) + дата и время
 dt = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-out = open('csv/vhi_id_11_{}.csv'.format(dt), 'wb')
+out = open('csv-raw/vhi_id_11_{}.csv'.format(dt), 'wb')
 
 # читаем URL и записываем в локальный файл
 out.write(vhi_url.read())
 out.close()
 
-print "Данные загружены в папку csv " + dt
+print "Данные загружены в папку csv-raw с текущей датой и временем:  " + dt
---
+++ get_url_data_ua.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+# Python скрипт для скачивания Средних данных для областей Украины: SMN, SMT, VCI, TCI, VHI
+# с сайта https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/vh_browseByCountry.php
+
