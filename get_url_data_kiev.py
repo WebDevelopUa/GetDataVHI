a35b047090b632c88adf0784102399244a3f924b
WebDev <js.develop.com@gmail.com>
2018-05-28 18:15:40 +0300

    Add procedures for forming samples

--- get_url_data_kiev.py
+++ get_url_data_kiev.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Python скрипт для скачивания Средних данных для Киева (ID=11): SMN, SMT, VCI, TCI, VHI
+# Python скрипт для скачивания Средних данных для Киевской области (ID=11): SMN, SMT, VCI, TCI, VHI
 # с сайта https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/vh_browseByCountry.php
 
 # urllib2 это модуль Python для работы с URL-адресом
@@ -7,7 +7,7 @@ import urllib2
 # datetime - модуль для работы с датой и временем
 import datetime
 
-# URL-адрес Киева (ID=11)
+# URL-адрес Киевской области (ID=11)
 url = "https://www.star.nesdis.noaa.gov/smcd/emb/vci/VH/get_provinceData.php?country=UKR&provinceID=11&year1=1981&year2=2018&type=Mean"
 
 print "Данные загружаются с сайта ... Примерное время ожидания - до 5 минут"
@@ -15,7 +15,7 @@ print "Данные загружаются с сайта ... Примерное 
 # функция urlopen() способна извлечь URL-адрес с помощью различных протоколов (HTTP, FTP)
 vhi_url = urllib2.urlopen(url)
 
-# откроем файл на запись (со скачиванием бинарного файла 'b') для Киева (ID=11) + дата и время
+# откроем файл на запись (со скачиванием бинарного файла 'b') для Киевской обласи (ID=11) + дата и время
 dt = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
 out = open('csv-raw/vhi_id_11_{}.csv'.format(dt), 'wb')
 
